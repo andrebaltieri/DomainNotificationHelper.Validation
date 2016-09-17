84bd9d762ab7f95d21bee2e78c8565405f07c875
tbaragao <tbaragao@gmail.com>
2016-09-17 18:39:42 -0300

    Atualização,correção e adição
    - Adição dos summary de todos o metodos Asserts para facilitar o objetivo do Assert;
    - Alteração do AssertNotEmpty de IsNullOrEmpty para IsNullOrWhiteSpace. IsNullOrWhiteSpace além de identificar se a string é null or empty ainda valida se ela é composta somente por espaços vazios;

--- src/AssertionConcern.cs
+++ src/AssertionConcern.cs
@@ -21,6 +21,14 @@ namespace DomainNotificationHelper.Validation
             notifications.ToList().ForEach(validation => { DomainEvent.Raise(validation); });
         }
 
+        /// <summary>
+        /// Assert if the string value respects the minimum and maximum limits size
+        /// </summary>
+        /// <param name="stringValue">String value</param>
+        /// <param name="minimum">Minimun limit size</param>
+        /// <param name="maximum">Maximun limit size</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertLength(string stringValue, int minimum, int maximum, string message)
         {
             var length = stringValue.Trim().Length;
@@ -30,22 +38,41 @@ namespace DomainNotificationHelper.Validation
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the string value matches with the Regex Pattern
+        /// </summary>
+        /// <param name="pattern">Regex Pattern</param>
+        /// <param name="stringValue">String value</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertMatches(string pattern, string stringValue, string message)
         {
             var regex = new Regex(pattern);
 
             return (!regex.IsMatch(stringValue))
-                ? new DomainNotification("AssertArgumentLength", message)
+                ? new DomainNotification("AssertArgumentMatches", message)
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the string value is not null, empty or white spaces
+        /// </summary>
+        /// <param name="stringValue">String Value</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertNotEmpty(string stringValue, string message)
         {
-            return (string.IsNullOrEmpty(stringValue))
+            return (string.IsNullOrWhiteSpace(stringValue))
                 ? new DomainNotification("AssertArgumentNotEmpty", message)
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the object is not null
+        /// </summary>
+        /// <param name="object1">Object to check</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertNotNull(object object1, string message)
         {
             return (object1 == null)
@@ -53,6 +80,12 @@ namespace DomainNotificationHelper.Validation
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the object is null
+        /// </summary>
+        /// <param name="object1">Object to check</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertIsNull(object object1, string message)
         {
             return (object1 != null)
@@ -60,6 +93,12 @@ namespace DomainNotificationHelper.Validation
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the validation statement or boolean value is true
+        /// </summary>
+        /// <param name="boolValue">Validation statement or boolean value</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertTrue(bool boolValue, string message)
         {
             return (!boolValue)
@@ -67,34 +106,83 @@ namespace DomainNotificationHelper.Validation
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the values are equals
+        /// </summary>
+        /// <param name="value">String value</param>
+        /// <param name="match">String value to match</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertAreEquals(string value, string match, string message)
         {
             return (!(value == match))
-                ? new DomainNotification("AssertArgumentTrue", message)
+                ? new DomainNotification("AssertArgumentEquals", message)
                 : null;
         }
 
+        /// <summary>
+        ///  Assert if the integer value 01 is bigger than integer value 02
+        /// </summary>
+        /// <param name="value1">Integer value 01</param>
+        /// <param name="value2">Integer value 02</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertIsGreaterThan(int value1, int value2, string message)
         {
             return (!(value1 > value2))
-                ? new DomainNotification("AssertArgumentTrue", message)
+                ? new DomainNotification("AssertArgumentGreatherThan", message)
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the decimal value 01 is bigger than decimal value 02
+        /// </summary>
+        /// <param name="value1">Decimal value 01</param>
+        /// <param name="value2">Decimal value 02</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertIsGreaterThan(decimal value1, decimal value2, string message)
         {
             return (!(value1 > value2))
-                ? new DomainNotification("AssertArgumentTrue", message)
+                ? new DomainNotification("AssertArgumentGreatherThan", message)
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the integer value 01 is bigger than or equals integer value 02
+        /// </summary>
+        /// <param name="value1">Integer value 01</param>
+        /// <param name="value2">Integer value 02</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertIsGreaterOrEqualThan(int value1, int value2, string message)
         {
             return (!(value1 >= value2))
-                ? new DomainNotification("AssertArgumentTrue", message)
+                ? new DomainNotification("AssertArgumentGreatherOrEqualThan", message)
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the decimal value 01 is bigger than decimal value 02
+        /// </summary>
+        /// <param name="value1">Decimal value 01</param>
+        /// <param name="value2">Decimal value 02</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
+        public static DomainNotification AssertIsGreaterOrEqualThan(decimal value1, decimal value2, string message)
+        {
+            return (!(value1 >= value2))
+                ? new DomainNotification("AssertArgumentGreatherOrEqualThan", message)
+                : null;
+        }
+
+        /// <summary>
+        /// Assert if the string value matches with Regex Pattern
+        /// </summary>
+        /// <param name="value">String value</param>
+        /// <param name="regex">Regex Pattern to match</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertRegexMatch(string value, string regex, string message)
         {
             return (!Regex.IsMatch(value, regex, RegexOptions.IgnoreCase))
@@ -102,6 +190,25 @@ namespace DomainNotificationHelper.Validation
                 : null;
         }
 
+        /// <summary>
+        /// Check if the Guid value is different of Guid.Empty
+        /// </summary>
+        /// <param name="value">Not nullable Guid</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
+        public static DomainNotification AssertGuidIsNotEmpty(Guid value, string message)
+        {
+            return (value == Guid.Empty)
+                ? new DomainNotification("AssertGuidIsEmpty", message)
+                : null;
+        }
+
+        /// <summary>
+        /// Assert if the string value is an valid email
+        /// </summary>
+        /// <param name="email">String value</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertEmailIsValid(string email, string message)
         {
             var emailRegex =
@@ -112,6 +219,12 @@ namespace DomainNotificationHelper.Validation
                 : null;
         }
 
+        /// <summary>
+        /// Assert if the string value is an valid url
+        /// </summary>
+        /// <param name="url">String value</param>
+        /// <param name="message">Message to return if Assert occurred</param>
+        /// <returns>DomainNotification object</returns>
         public static DomainNotification AssertUrlIsValid(string url, string message)
         {
             // Do not validate if no URL is provided
